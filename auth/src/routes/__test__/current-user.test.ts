c76e3b7c2c99ff1378fba15df3c2c08717e0805d
David Song <deokwons9004dev@gmail.com>
2023-06-21 13:38:19 +0900

    fixfixfix

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -18,8 +18,7 @@ it('responds with details about the current user', async () => {
 		.get('/api/users/currentuser')
 		.set('Cookie', cookie)
 		.send()
-		.expect(400);
-		// .expect(200);
+		.expect(200);
 		
 	expect(res.body.currentUser.email).toEqual('test@test.com');
 });
